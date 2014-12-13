bd73bb3653d6abc158fef67c17362da4c4fc1d89
ripng <matthewalex@outlook.com>
2014-12-13 01:29:22 -0500

    cpufreq: lazy:3.10 fixup

--- include/asm-generic/cputime.h
+++ include/asm-generic/cputime.h
@@ -7,7 +7,7 @@
 #ifndef CONFIG_VIRT_CPU_ACCOUNTING
 # include <asm-generic/cputime_jiffies.h>
 #endif
-
+#define cputime64_sub(__a, __b) ((__a) - (__b)) 
 #ifdef CONFIG_VIRT_CPU_ACCOUNTING_GEN
 # include <asm-generic/cputime_nsecs.h>
 #endif
