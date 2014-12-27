a5dd9eceee2b252ff2a02bf12cb82f3f3887fffe
RIPng <matthewalex@outlook.com>
2014-12-27 17:57:00 -0500

    build: change tc

--- sensei-build.sh
+++ sensei-build.sh
@@ -16,12 +16,12 @@ DEFCONFIG="shamu_defconfig"
 
 # Kernel Details
 BASE_AK_VER="SENSEI"
-VER=".R25"
+VER=".R28"
 AK_VER="$BASE_AK_VER$VER"
 
 # Vars
 export LOCALVERSION=~`echo $AK_VER`
-export CROSS_COMPILE=${HOME}/android/AK-uber-arm-aebi-4.9/bin/arm-eabi-
+export CROSS_COMPILE=${HOME}/android/Sabermod/bin/arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=RIPng
