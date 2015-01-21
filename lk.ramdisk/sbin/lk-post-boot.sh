26beaac27a9f5eb842906014ba32c2efd4011d4a
RIPng <matthewalex@outlook.com>
2015-01-21 15:31:19 -0500

    Use LK ramdisk

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -11,12 +11,10 @@ chmod 444 /dev/frandom
 	"allow untrusted_app persist_file dir { open read getattr }" \
 	"allow debuggerd gpu_device chr_file { open read getattr }" \
 	"allow netd netd capability fsetid" \
-	"allow netd { hostapd dnsmasq } process fork" \
 	"allow { system_app shell } dalvikcache_data_file file write" \
 	"allow { zygote mediaserver bootanim appdomain }  theme_data_file dir { search r_file_perms r_dir_perms }" \
 	"allow { zygote mediaserver bootanim appdomain }  theme_data_file file { r_file_perms r_dir_perms }" \
-	"allow system_server { rootfs resourcecache_data_file } dir { open read write getattr add_name setattr create remove_name rmdir unlink link }"\
-	"allow system_server resourcecache_data_file file { open read write getattr add_name setattr create remove_name unlink link }"
+	"allow system_server rootfs dir { open read write getattr }"
 
 # take a little more RAM from file/dir caches and give them to apps 
 echo 200 > /proc/sys/vm/vfs_cache_pressure
